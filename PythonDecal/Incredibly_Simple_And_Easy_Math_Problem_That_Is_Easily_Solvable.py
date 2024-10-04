6bd932669405c1e80a777087a72fc3c7adf1085a
Lobomb <clobo@berkeley.edu>
2024-10-03 17:15:36 -0700

    Add files via upload

---
+++ PythonDecal/Incredibly_Simple_And_Easy_Math_Problem_That_Is_Easily_Solvable.py
@@ -0,0 +1,4 @@
+print((54719847**13)/312334 +381092480*18034-29)
+#fun math problem that should be easily solvable without a calculator and should take 30 seconds max to solve. 
+#Thought process: Make big number. Reason for name:Read first comment^
+#Command to run file: python Incredibly_Simple_And_Easy_Math_Problem_That_Is_Easily_Solvable.py
